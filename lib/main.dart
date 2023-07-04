89ee8105c7be91abbb4778c89399bc536a2f677b
tintin <tinashekeith24@gmail.com>
2023-07-04 19:29:53 +0200

    added sentry to staging and prod environments

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 import 'src/app.dart';
 
 void main() async {
-  runApp(const App());
+  const sentryEnvironment = "prod";
+
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          "https://e20157210f3d448eb61748c76fdd8581@o4505472229179392.ingest.sentry.io/4505472298516480";
+      options.tracesSampleRate = 1.0;
+      options.environment = sentryEnvironment;
+    },
+    appRunner: () => runApp(
+      const App(),
+    ),
+  );
 }
