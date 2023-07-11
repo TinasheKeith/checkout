69044ffe4cab5f6bb8fb2e8704ecc049f7926aef
tintin <tinashekeith24@gmail.com>
2023-07-11 12:28:52 +0200

    fixed linting issues

--- lib/main_stg.dart
+++ lib/main_stg.dart
@@ -1,10 +1,14 @@
 import 'package:checkout/src/app.dart';
 import 'package:checkout/src/constants.dart';
+import 'package:checkout/src/services/db_service.dart';
 import 'package:flutter/material.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   const sentryEnvironment = 'staging';
+
+  // ignore: unused_local_variable
+  final dbService = DatabaseService();
 
   await SentryFlutter.init(
     (options) {
