0155a2a0fec798646043605238901c48289ea127
Tinashe Keith Madzimbamuto <Tinashekeith24@gmail.com>
2023-07-11 02:39:51 +0200

    added dashboard screen (#10)

--- lib/asset_paths.dart
+++ lib/asset_paths.dart
@@ -2,10 +2,19 @@ import 'package:flutter/material.dart';
 
 @immutable
 class Assets {
+  /*
+    *** LOGO IMAGES ***
+  */
   static const visaLogo = 'assets/images/provider_logos/visa.webp';
   static const americanExpressLogo =
       'assets/images/provider_logos/american_express.webp';
   static const mastercardLogo = 'assets/images/provider_logos/card.webp';
   static const unionPayLogo = 'assets/images/provider_logos/union_pay.webp';
   static const discoverLogo = 'assets/images/provider_logos/discover.webp';
+
+  /*
+    *** LOTTIE ANIMATIONS ***
+  */
+  static const cardLight = 'assets/lottis/card_light.json';
+  static const cardDark = 'assets/lottis/card_dark.json';
 }
