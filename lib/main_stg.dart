173c6200f6615a3cc570addf313fadcdd93223f4
tintin <tinashekeith24@gmail.com>
2023-07-12 10:06:02 +0200

    maintenence

--- lib/main_stg.dart
+++ lib/main_stg.dart
@@ -1,14 +1,11 @@
 import 'package:checkout/main.dart';
 import 'package:checkout/src/app.dart';
-import 'package:checkout/src/constants.dart';
 import 'package:checkout/src/locator.dart';
 import 'package:checkout/src/models/checkout_card_model.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
-  const sentryEnvironment = 'staging';
 
   setupLocator(AppFlavor.staging);
 
@@ -16,15 +13,5 @@ void main() async {
 
   await Hive.initFlutter();
 
-  await SentryFlutter.init(
-    (options) {
-      options
-        ..dsn = sentryDsn
-        ..tracesSampleRate = 1.0
-        ..environment = sentryEnvironment;
-    },
-    appRunner: () => runApp(
-      const App(),
-    ),
-  );
+  runApp(const App());
 }
